fd1925048b0108f9184579cf0cee03d4906cab06
shubhadipmaity <shubhadip.maity@paytminsurance.co.in>
2021-09-27 19:46:57 +0530

    feat: container deployment added

---
+++ marketing/config/webpack.prod.js
@@ -0,0 +1,26 @@
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
+
+const prodConfig = {
+  mode: 'production',
+  output: {
+    filename: '[name].[contenthash].js'
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'marketing',
+      exposes: {
+        './MarketingApp': './src/bootstrap',
+      },
+      shared: packageJson.dependencies,
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, prodConfig);
